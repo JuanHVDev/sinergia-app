7830175480fe689ecdec953ddd10e7edfe38661c
Juan Herrera Vázquez <jherrerav1901@alumno.ipn.mx>
2025-03-06 23:31:12 -0600

    Resolver conflicto de fusión en la página de Organizaciones

--- app/(privada)/organization/page.tsx
+++ app/(privada)/organization/page.tsx
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
 import { Button } from "@/components/ui/button";
 
-export default async function AppPage() {
-    // const session = await auth.api.getSession({
-    //     headers: await headers(),
-    // });
-    // if (!session) {
-    //     redirect("/login");
-    // }
+export default async function OrganizationPage() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-2xl font-bold">Organizaciones </h1>
-=======
-export default async function AppPage() {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold">Organizaciones</h1>
->>>>>>> 2c2e5e5d0cfce67919085386ba176ad52e759fdb
         </div>
     );
 }
